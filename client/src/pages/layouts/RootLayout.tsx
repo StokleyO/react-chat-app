bc56c45c6a0a88c2e6beae5f578521546e3be348
Stokley Onuba <StokleyOnubaBusiness@gmail.com>
2023-02-11 18:13:25 -0500

    Last Train To Brooklyn

---
+++ client/src/pages/layouts/RootLayout.tsx
@@ -0,0 +1,13 @@
+import { Navigate, Outlet } from 'react-router-dom';
+import { FullScreenCard } from '../../components/FullScreenCard';
+import { useAuth } from '../../context/AuthContext';
+
+export function RootLayout() {
+    const { user } = useAuth()
+
+    if (user == null ) return <Navigate to="/login" />
+    
+    return (
+         <Outlet />
+    )
+}
